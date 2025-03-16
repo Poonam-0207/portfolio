3c816ab4618445244e7cd3e170f2425c868bc402
Punam <punam@email.com>
2025-03-16 19:52:51 +0530

    ADDED FILES

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Live Date & Time
 function updateTime() {
     const now = new Date();
     document.getElementById('current-time').innerText = now.toLocaleString();
@@ -5,6 +6,7 @@ function updateTime() {
 setInterval(updateTime, 1000);
 updateTime();
 
+// Contact Form Validation with Success Message
 function validateForm(event) {
     event.preventDefault();
     
